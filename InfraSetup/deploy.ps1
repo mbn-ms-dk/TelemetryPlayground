146bc1f3a267adc132ba41cba627b67f35dfc42c
Mikkel Bach Nielsen <minielsen@microsoft.com>
2022-09-23 10:18:40 +0200

    change baseline name

--- InfraSetup/deploy.ps1
+++ InfraSetup/deploy.ps1
@@ -1,5 +1,5 @@
-$baseline="mbn"
-$location="westeurope"
+$baseline="demo"
+$location="northeurope"
 
 # Create deployment
 az deployment sub create -n "Dep-$baseline" -l $location -f main.bicep --parameters baseName=$baseline --parameters location=$location
